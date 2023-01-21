42401b713e2fdbe430c6bf4c5de034d9fcd38d8c
Lemon Soju <yckk980320@naver.com>
2023-01-21 19:07:29 +0900

    response speed check 4-4

--- 반응속도/client.jsx
+++ 반응속도/client.jsx
@@ -1,5 +1,8 @@
 const React = require('react');
 const ReactDom = require('react-dom');
-const { default: ResponseCheck } = require('./ResponseCheck');  
+// const { default: ResponseCheck } = require('./ResponseCheck');  
+const { default: ResponseCheckHook } = require('./ResponseCheckHook');  
 
-ReactDom.render(<ResponseCheck />, document.querySelector('#root'));
+
+// ReactDom.render(<ResponseCheck />, document.querySelector('#root'));
+ReactDom.render(<ResponseCheckHook />, document.querySelector('#root'));
