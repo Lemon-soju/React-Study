c4cdaa991b60a076184592ac5e6127d0a21e1836
Lemon Soju <yckk980320@naver.com>
2023-01-23 15:48:36 +0900

    Lotto 6-2

--- Lotto/client.jsx
+++ Lotto/client.jsx
@@ -1,9 +1,8 @@
 const React = require('react');
 const ReactDom = require('react-dom');
-// const { default: RSP } = require('./RSP');  
-const { default: RSP2_Hooks } = require('./RSP2_Hooks');  
+const { default: Lotto } = require('./Lotto.jsx');  
 
 
 // ReactDom.render(<RSP />, document.querySelector('#root'));
 // ReactDom.render(<RSP2_Hooks />, document.querySelector('#root')); // 17 버전
-ReactDom.createRoot(document.querySelector('#root')).render(<RSP2_Hooks />); // 18 버전
+ReactDom.createRoot(document.querySelector('#root')).render(<Lotto />); // 18 버전
