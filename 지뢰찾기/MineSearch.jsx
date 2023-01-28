b1ff2f07da67e477608c8b05d730f76719a78c41
Lemon Soju <yckk980320@naver.com>
2023-01-28 13:51:38 +0900

    Search Mine 8-6

--- 지뢰찾기/MineSearch.jsx
+++ 지뢰찾기/MineSearch.jsx
@@ -21,19 +21,24 @@ export const TableContext = createContext({
 
 const initialState = {
   tableData: [],
-  timer: 0, 
+  timer: 0,
   result: "",
   halted: true,
 };
 
 const plantMine = (row, cell, mine) => {
   console.log(row, cell, mine);
-  const candidate = Array(row * cell).fill().map((arr, i) => {
-    return i;
-  });
+  const candidate = Array(row * cell)
+    .fill()
+    .map((arr, i) => {
+      return i;
+    });
   const shuffle = [];
   while (candidate.length > row * cell - mine) {
-    const chosen = candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0];
+    const chosen = candidate.splice(
+      Math.floor(Math.random() * candidate.length),
+      1
+    )[0];
     shuffle.push(chosen);
   }
   const data = [];
@@ -55,13 +60,13 @@ const plantMine = (row, cell, mine) => {
   return data;
 };
 
-export const START_GAME = 'START_GAME';
-export const OPEN_CELL = 'OPEN_CELL';
-export const CLICK_MINE = 'CLICK_MINE';
-export const FLAG_CELL = 'FLAG_CELL';
-export const QUESTION_CELL = 'QUESTION_CELL';
-export const NORMALIZE_CELL = 'NORMALIZE_CELL';
-export const INCREMENT_TIMER = 'INCREMENT_TIMER';
+export const START_GAME = "START_GAME";
+export const OPEN_CELL = "OPEN_CELL";
+export const CLICK_MINE = "CLICK_MINE";
+export const FLAG_CELL = "FLAG_CELL";
+export const QUESTION_CELL = "QUESTION_CELL";
+export const NORMALIZE_CELL = "NORMALIZE_CELL";
+export const INCREMENT_TIMER = "INCREMENT_TIMER";
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -73,62 +78,147 @@ const reducer = (state, action) => {
       };
     case OPEN_CELL: {
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      tableData[action.row][action.cell] = CODE.OPENED;
-      return {        
+      tableData.forEach((row, i) => {
+        tableData[i] = [...state.tableData[i]];
+      });
+
+      const checked = [];
+      const checkAround = (row, cell) => {
+        if (
+          // 닫힌 칸만 열기
+          [
+            CODE.OPENED,
+            CODE.FLAG_MINE,
+            CODE.FLAG,
+            CODE.QUESTION_MINE,
+            CODE.QUESTION,
+          ].includes(tableData[row][cell])
+        ) {
+          return;
+        }
+        if (
+          // 상하좌우 없는칸은 안결기
+          row < 0 ||
+          row >= tableData.length ||
+          cell < 0 ||
+          cell >= tableData[0].length
+        ) {
+          return;
+        }
+        if (checked.includes(row + "/" + cell)) {
+          // 한번 연칸은 무시하기
+          return;
+        } else {
+          checked.push(row + "/" + cell);
+        }
+        let around = [];
+        if (tableData[row - 1]) {
+          around = around.concat(
+            tableData[row - 1][cell - 1],
+            tableData[row - 1][cell],
+            tableData[row - 1][cell + 1]
+          );
+        }
+        around = around.concat(
+          tableData[row][cell - 1],
+          tableData[row][cell + 1]
+        );
+        if (tableData[row + 1]) {
+          around = around.concat(
+            tableData[row + 1][cell - 1],
+            tableData[row + 1][cell],
+            tableData[row + 1][cell + 1]
+          );
+        }
+        const count = around.filter((v) =>
+          [CODE.MINE, CODE.FLAG_MINE, CODE.QUESTION_MINE].includes(v)
+        ).length;
+        tableData[row][cell] = count;
+        if (count === 0) {
+          const near = [];
+          if (row - 1 > -1) {
+            near.push([row - 1, cell - 1]);
+            near.push([row - 1, cell]);
+            near.push([row - 1, cell + 1]);
+          }
+          near.push([row, cell - 1]);
+          near.push([row, cell + 1]);
+          if (row + 1 < tableData.length) {
+            near.push([row + 1, cell - 1]);
+            near.push([row + 1, cell]);
+            near.push([row + 1, cell + 1]);
+          }
+          near.forEach((n) => {
+            if (tableData[n[0]][n[1]] !== CODE.OPENED) {
+              checkAround(n[0], n[1]);
+            }
+          });
+        } else {
+        }
+      };
+      checkAround(action.row, action.cell);
+      return {
         ...state,
         tableData,
-      }
+      };
     }
     case CLICK_MINE: {
+      const row = action.row;
+      const cell = action.cell;
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      tableData[action.row][action.cell] = CODE.CLICKED_MINE;
+      tableData[row] = [...state.tableData[row]];
+      tableData[row][cell] = CODE.CLICKED_MINE;
       return {
         ...state,
         tableData,
         halted: true,
-      }
+      };
     }
     case FLAG_CELL: {
+      const row = action.row;
+      const cell = action.cell;
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      if ( tableData[action.row][action.cell] === CODE.MINE) {
-        tableData[action.row][action.cell] = CODE.FLAG_MINE;
+      tableData[row] = [...state.tableData[row]];
+      if (tableData[row][cell] === CODE.MINE) {
+        tableData[row][cell] = CODE.FLAG_MINE;
       } else {
-        tableData[action.row][action.cell] = CODE.FLAG;
+        tableData[row][cell] = CODE.FLAG;
       }
       return {
         ...state,
         tableData,
-      }
+      };
     }
     case QUESTION_CELL: {
+      const row = action.row;
+      const cell = action.cell;
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      if ( tableData[action.row][action.cell] === CODE.FLAG_MINE) {
-        tableData[action.row][action.cell] = CODE.QUESTION_MINE;
+      tableData[row] = [...state.tableData[row]];
+      if (tableData[row][cell] === CODE.FLAG_MINE) {
+        tableData[row][cell] = CODE.QUESTION_MINE;
       } else {
-        tableData[action.row][action.cell] = CODE.QUESTION;
+        tableData[row][cell] = CODE.QUESTION;
       }
       return {
         ...state,
         tableData,
-      }
-    } 
+      };
+    }
     case NORMALIZE_CELL: {
+      const row = action.row;
+      const cell = action.cell;
       const tableData = [...state.tableData];
-      tableData[action.row] = [...state.tableData[action.row]];
-      if ( tableData[action.row][action.cell] ===  CODE.QUESTION_MINE) {
-        tableData[action.row][action.cell] = CODE.MINE;
+      tableData[row] = [...state.tableData[row]];
+      if (tableData[row][cell] === CODE.QUESTION_MINE) {
+        tableData[row][cell] = CODE.MINE;
       } else {
-        tableData[action.row][action.cell] = CODE.NORMAL;
+        tableData[row][cell] = CODE.NORMAL;
       }
       return {
         ...state,
         tableData,
-      }
-    } 
+      };
+    }
     default:
       return state;
   }
@@ -137,10 +227,10 @@ const reducer = (state, action) => {
 const MineSearch = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { tableData, halted, timer, result } = state;
-  
+
   const value = useMemo(
     () => ({ tableData: tableData, halted: halted, dispatch }),
-    [tableData, halted] 
+    [tableData, halted]
   );
 
   return (
--- 지뢰찾기/Td.jsx
+++ 지뢰찾기/Td.jsx
@@ -1,52 +1,60 @@
 import React, { useContext, useCallback } from "react";
-import { CLICK_MINE, CODE, FLAG_CELL, OPEN_CELL, QUESTION_CELL, TableContext, NORMALIZE_CELL } from "./MineSearch";
+import {
+  CLICK_MINE,
+  CODE,
+  FLAG_CELL,
+  OPEN_CELL,
+  QUESTION_CELL,
+  TableContext,
+  NORMALIZE_CELL,
+} from "./MineSearch";
 
 const getTdStyle = (code) => {
   switch (code) {
     case CODE.NORMAL:
     case CODE.MINE:
       return {
-        background: '#444',
+        background: "#444",
       };
     case CODE.CLICKED_MINE:
     case CODE.OPENED:
       return {
-        background: 'white',
+        background: "white",
       };
     case CODE.QUESTION_MINE:
     case CODE.QUESTION:
       return {
-        background: 'yellow',
+        background: "yellow",
       };
     case CODE.FLAG_MINE:
     case CODE.FLAG:
       return {
-        background: 'red',
+        background: "red",
       };
     default:
       return {
-        background: 'white',
+        background: "white",
       };
   }
 };
 
 const getTdText = (code) => {
-  console.log('getTdtext');
+  console.log("getTdtext");
   switch (code) {
     case CODE.NORMAL:
-      return '';
+      return "";
     case CODE.MINE:
-      return 'X';
+      return "X";
     case CODE.CLICKED_MINE:
-      return '펑';
+      return "펑";
     case CODE.FLAG_MINE:
     case CODE.FLAG:
-      return '!';
+      return "!";
     case CODE.QUESTION_MINE:
     case CODE.QUESTION:
-      return '?';
+      return "?";
     default:
-      return code || '';
+      return code || "";
   }
 };
 const Td = ({ rowIndex, cellIndex }) => {
@@ -74,33 +82,38 @@ const Td = ({ rowIndex, cellIndex }) => {
     }
   }, [tableData[rowIndex][cellIndex], halted]);
 
-  const onRightClickTd = useCallback((e) => {
-    e.preventDefault();
-    if (halted) {
-      return;
-    }
-    switch (tableData[rowIndex][cellIndex]) {
-      case CODE.NORMAL:
-      case CODE.MINE:
-        dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+  const onRightClickTd = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (halted) {
         return;
-      case CODE.FLAG_MINE:
-      case CODE.FLAG:
-        dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
-        return;
-      case CODE.QUESTION_MINE:
-      case CODE.QUESTION:
-        dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
-        return;
-      default:
-        return;
-    }
-  }, [tableData[rowIndex][cellIndex], halted])
+      }
+      switch (tableData[rowIndex][cellIndex]) {
+        case CODE.NORMAL:
+        case CODE.MINE:
+          dispatch({ type: FLAG_CELL, row: rowIndex, cell: cellIndex });
+          return;
+        case CODE.FLAG_MINE:
+        case CODE.FLAG:
+          dispatch({ type: QUESTION_CELL, row: rowIndex, cell: cellIndex });
+          return;
+        case CODE.QUESTION_MINE:
+        case CODE.QUESTION:
+          dispatch({ type: NORMALIZE_CELL, row: rowIndex, cell: cellIndex });
+          return;
+        default:
+          return;
+      }
+    },
+    [tableData[rowIndex][cellIndex], halted]
+  );
 
   return (
-    <td style={getTdStyle(tableData[rowIndex][cellIndex])}
-    onClick={onClickTd}
-    onContextMenu={onRightClickTd}>
+    <td
+      style={getTdStyle(tableData[rowIndex][cellIndex])}
+      onClick={onClickTd}
+      onContextMenu={onRightClickTd}
+    >
       {getTdText(tableData[rowIndex][cellIndex])}
     </td>
   );
